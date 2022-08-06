fb63e1ade1963c07876dd0063612785f5293a0e8
Tatiana <lulu.kep@yandex.ru>
2022-08-06 21:42:35 +0400

    034

---
+++ 034/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter size of array: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int j = 0;
+for (int i = 0; i < n; i++)
+{
+    arr[i] = new Random().Next(100 , 1000);
+    if (arr[i] % 2 == 0) j++;
+}
+Console.WriteLine();
+Console.WriteLine("[" + string.Join(", ", arr) + "]");
+Console.WriteLine("There are/is "+ j + " even number(s)");
