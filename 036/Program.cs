de87ca7c87e969ec3241368e4330414e002f08e7
Tatiana <lulu.kep@yandex.ru>
2022-08-06 22:03:13 +0400

    036

---
+++ 036/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter size of array: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[n];
+int sum = 0;
+for (int i = 0; i < n; i++)
+{
+    arr[i] = new Random().Next(-1000, 1000);
+    if (i % 2 != 0) sum = sum + arr[i];
+}
+
+Console.WriteLine();
+Console.WriteLine("[" + string.Join(", ", arr) + "]");
+Console.WriteLine("Sum is = "+ sum);
